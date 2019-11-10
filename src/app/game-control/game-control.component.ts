b4a824d40b815bc94320a329d0962f9822e9af4b
Onder Genc <ondergenc@gmail.com>
2019-11-10 19:04:54 +0300

    Added EventEmitter in the game-control. And Value in game-control was passed to app-component with EventEmit.

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,7 +6,7 @@ import { Component, OnInit, EventEmitter } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
-  intervalFired = new EventEmitter<number>();
+  @Output() intervalFired = new EventEmitter<number>();
   interval;
   lastNumber = 0;
 
